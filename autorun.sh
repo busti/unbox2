75854ac5d43073dc5c4d46e3cf62906453f9df46
busti <git@busti.cool>
2021-01-29 22:29:42 +0100

    recurse submodules

--- autorun.sh
+++ autorun.sh
@@ -2,11 +2,11 @@ echo -n "git repository url: "
 read remote
 echo $remote
 
-git clone "$remote" repo
+git clone --recurse-submodules "$remote" repo
 
 if [[ ! -e repo/entry.sh ]]; then
   echo no entry found;
   exit 1;
 fi
 
-sudo /bin/bash repo/entry.sh
+sudo /bin/sh repo/entry.sh
